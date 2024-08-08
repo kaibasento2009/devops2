3f0047a1fd26d9dbecc97ff819f7998be77f2ce7
kaibasento2009 <50627222+kaibasento2009@users.noreply.github.com>
2024-08-08 10:02:42 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_instance" "vm" {
     ami = var.ami
     instance_type = var.instance-type
     vpc_security_group_ids = [ aws_security_group.security-group.id ]
-    key_name = aws_key_pair.Taidemokeypair1.id
+    key_name = Taidemokeypair1
     user_data = "${file("install_nginx.sh")}"
     tags = {
         server = "dc1-webserver"
