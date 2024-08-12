9002684017f56085cb7c7acf41ba4468f6a161d3
kaibasento2009 <50627222+kaibasento2009@users.noreply.github.com>
2024-08-12 17:19:36 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -118,8 +118,8 @@ resource "aws_eip" "elastic-ip" {
     instance = aws_instance.vm.id 
 }
 
-resource "aws_key_pair" "demoenvkey" {
-  key_name   = "demoenvkey"
+resource "aws_key_pair" "demoenvkey-Tai01" {
+  key_name   = "demoenvkey-Tai01"
   public_key = var.public-key
 }
 
@@ -128,7 +128,7 @@ resource "aws_instance" "vm" {
     ami = var.ami
     instance_type = var.instance-type
     vpc_security_group_ids = [ aws_security_group.security-group.id ]
-    key_name = aws_key_pair.demoenvkey.id
+    key_name = aws_key_pair.demoenvkey-Tai01.id
     user_data = "${file("install_nginx.sh")}"
     tags = {
         server = "dc1-webserver"
