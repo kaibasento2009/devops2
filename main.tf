9e5f97e9f48809019c527014d47ab674d8e6f2c6
kaibasento2009 <50627222+kaibasento2009@users.noreply.github.com>
2024-08-12 18:52:15 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -156,10 +156,3 @@ resource "aws_db_instance" "database" {
     }
 
 }
-
-
-
-//resource "aws_internet_gateway_attachment" "internet-gateway-attachment" {
-//    internet_gateway_id = aws_internet_gateway.internet-gateway.id
-//    vpc_id = aws_vpc.tf-testing-vpc.id
-//}
